4768791d6984d0b8017ee71dfc1b478b0f91df77
Darius Matthews <Dariuscmatthews@gmail.com>
2020-01-24 15:18:12 -0600

    Updates

---
+++ precache-manifest.7d6680017cbb6665e0221542b55d0bff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f236430eee5420af47ccc95ccf71022",
+    "url": "/Countries_landing/index.html"
+  },
+  {
+    "revision": "7a39a870f49ee5483966",
+    "url": "/Countries_landing/static/css/main.c3f232ca.chunk.css"
+  },
+  {
+    "revision": "e657b6f43d71fb1cdfd4",
+    "url": "/Countries_landing/static/js/2.33bb0505.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Countries_landing/static/js/2.33bb0505.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a39a870f49ee5483966",
+    "url": "/Countries_landing/static/js/main.ee3a1479.chunk.js"
+  },
+  {
+    "revision": "9cf7371ecb2153e942f3",
+    "url": "/Countries_landing/static/js/runtime-main.64b23685.js"
+  },
+  {
+    "revision": "713fe29d10415a51910d79f66c21d161",
+    "url": "/Countries_landing/static/media/arrow-left.713fe29d.svg"
+  },
+  {
+    "revision": "399b1420858b9b19d9046a66f05227a8",
+    "url": "/Countries_landing/static/media/dark-mode-moon.399b1420.svg"
+  },
+  {
+    "revision": "2c0862a67bfb8773d52c7833e17cd5ca",
+    "url": "/Countries_landing/static/media/down-btn.2c0862a6.svg"
+  },
+  {
+    "revision": "6f1f85f3a1c1a07ace28f724b77de4c2",
+    "url": "/Countries_landing/static/media/light-mode-moon.6f1f85f3.svg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/Countries_landing/static/media/search-glass.c6a0effb.svg"
+  }
+]);
